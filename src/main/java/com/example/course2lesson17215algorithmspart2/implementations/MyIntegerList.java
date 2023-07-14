058e22b2a2918828397618fbf3c5099a178e68dc
Antonov333 <antonovsergei333@yahoo.com>
2023-07-14 21:58:23 +0300

    Measured time of sorting arrays by different algorithms

--- src/main/java/com/example/course2lesson17215algorithmspart2/implementations/MyIntegerList.java
+++ src/main/java/com/example/course2lesson17215algorithmspart2/implementations/MyIntegerList.java
@@ -9,10 +9,10 @@ import java.util.stream.IntStream;
 public class MyIntegerList implements IntegerList {
 
     private Integer[] storage;
-    private int count ; // Will make it auto expanding
+    private int count; // Will make it auto expanding
 
     public MyIntegerList() {
-        this.storage = new Integer[100] ;
+        this.storage = new Integer[100];
         count = 0;
     }
 
@@ -20,31 +20,40 @@ public class MyIntegerList implements IntegerList {
         if (capacity < 1) {
             throw new IntegerListException("capacity invalid");
         }
-        this.storage = new Integer[capacity] ;
+        this.storage = new Integer[capacity];
         count = 0;
     }
 
+    public MyIntegerList(Integer[] array) {
+        storage = array;
+        count = array.length;
+    }
+
     public int getCapacity() {
-        return storage.length ;
+        return storage.length;
     }
 
     @Override
     public Integer add(Integer item) {
-        if (item == null) {throw new IntegerListException("null item detected") ; }
-        int l = storage.length ;
-        if (count>=l) {this.doubleStorage();}
-        storage[count] = item ;
-        count++ ;
+        if (item == null) {
+            throw new IntegerListException("null item detected");
+        }
+        int l = storage.length;
+        if (count >= l) {
+            this.doubleStorage();
+        }
+        storage[count] = item;
+        count++;
         return item;
     }
 
     private void doubleStorage() {
-        int lengthOfExistingArray = storage.length ;
-        Integer[] storageDoubled = new Integer[lengthOfExistingArray * 2] ;
+        int lengthOfExistingArray = storage.length;
+        Integer[] storageDoubled = new Integer[lengthOfExistingArray * 2];
         for (int i = 0; i < lengthOfExistingArray; i++) {
-            storageDoubled[i] = storage[i] ;
+            storageDoubled[i] = storage[i];
         }
-        storage = storageDoubled ;
+        storage = storageDoubled;
     }
 
     private void checkItem(Integer item) {
@@ -67,59 +76,94 @@ public class MyIntegerList implements IntegerList {
 
     @Override
     public Integer add(int index, Integer item) {
+
+        checkIndexAndItem(index, item);
+
         Integer toReturn = item;
-        checkIndexAndItem(index + 1, item);
-        if(count>=storage.length) {this.doubleStorage();}
-        IntStream.iterate(count - 1, i -> i >= index, i -> i - 1).forEach(i -> storage[i + 1] = storage[i]);
-        count++;
+        if (count + 1 >= storage.length) {
+            this.doubleStorage();
+        }
+        if (index == count - 1) {
+            storage[count] = storage[index];
+        } else {
+            IntStream.iterate(count - 1, i -> i >= index, i -> i - 1).forEach(i -> storage[i + 1] = storage[i]);
+        }
         storage[index] = item;
+        count++;
+
         return toReturn;
     }
 
     @Override
     public Integer set(int index, Integer item) {
         checkIndexAndItem(index, item);
-        storage[index] = item ;
+        storage[index] = item;
         return item;
     }
 
     @Override
     public Integer remove(Integer item) {
         Integer toReturn = null;
-
         for (int i = 0; i < count; i++) {
             if (storage[i] == item) {
-                toReturn = item ;
-                for (int j = i; j < count; j++) {
-                    storage[j] = storage [j+1];
+                toReturn = item;
+                for (int j = i; j < count - 1; j++) {
+                    storage[j] = storage[j + 1];
                 }
-                storage[count-1] = null ;
-                count-- ;
+                storage[count - 1] = null;
+                count--;
                 break;
             }
         }
-
         return toReturn;
     }
 
     @Override
     public Integer remove(int index) {
-        return null;
+        checkIndex(index);
+        Integer item = storage[index];
+        for (int j = index; j < count - 1; j++) {
+            storage[j] = storage[j + 1];
+        }
+        storage[count - 1] = null;
+        count--;
+        return item;
     }
 
     @Override
     public boolean contains(Integer item) {
+        for (Integer i : storage) {
+            if (i.equals(item)) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    private void insert(int index, Integer item) {
+
     }
 
     @Override
     public int indexOf(Integer item) {
-        return 0;
+        int index = 0;
+        for (index = 0; index < count; index++) {
+            if (item.equals(storage[index])) {
+                return index;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Integer item) {
-        return 0;
+        int index = count - 1;
+        for (; index >= 0; index--) {
+            if (item.equals(storage[index])) {
+                return index;
+            }
+        }
+        return -1;
     }
 
     @Override
@@ -129,7 +173,15 @@ public class MyIntegerList implements IntegerList {
 
     @Override
     public boolean equals(IntegerList otherList) {
-        return false;
+        boolean result = count == otherList.size();
+        if (result) {
+            for (int i = 0; i < count - 1; i++) {
+                if (!storage[i].equals(otherList.get(i))) {
+                    return false;
+                }
+            }
+        }
+        return result;
     }
 
     @Override
@@ -139,16 +191,91 @@ public class MyIntegerList implements IntegerList {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return count == 0;
     }
 
     @Override
     public void clear() {
-
+        Integer[] clearStorage = new Integer[count];
+        storage = clearStorage;
+        count = 0;
     }
 
     @Override
     public Integer[] toArray() {
-        return new Integer[0];
+        final Integer[] storage1 = storage;
+        return storage1;
     }
+
+    public void sortByInsertion() {
+        if (count <= 1) {
+            return;
+        }
+
+        Integer key;
+        for (int i = 1; i < count; i++) {
+            key = storage[i];
+            for (int j = i; j > 0; j--) {
+                if (storage[j - 1] > key) {
+                    swapItems(j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+        return;
+    }
+
+    public void sortBySelection() {
+        Integer element = storage[0];
+
+        for (int i = 0; i < count - 2; i++) {
+            element = storage[i];
+            int iom = getIndexOfMin(storage, i + 1);
+            if (element > storage[iom]) {
+                swapItems(i, iom);
+            }
+        }
+    }
+
+    public void bubbleSort() {
+        int length = storage.length ;
+        if (length < 2) { return ; }
+        for (int i = 0; i < length - 1 ; i++) {
+            for (int j = 0 ; j < length-1-i ; j++) {
+                if (storage[j] > storage[j+1]) {
+                    swapItems(j,j+1);
+                }
+            }
+        }
+    }
+
+    private int getIndexOfMin(Integer[] array, int startIndex) {
+        int length = array.length;
+        if (startIndex < 0 || startIndex >= length) { throw new IntegerListException("startIndex invalid"); }
+        if (array == null || length == 0) { throw new IntegerListException("array is null or empty"); }
+
+        if (length == 1) return 0;
+
+        Integer minimum = array[0];
+        int indexOfMin = 0;
+        for (int i = 1; i < array.length - 1; i++) {
+            if (array[i] < minimum) {
+                minimum = array[i];
+                indexOfMin = i;
+            }
+        }
+        return indexOfMin;
+    }
+
+    private void swapItems(int indexA, int indexB) {
+        Integer tmp = storage[indexA];
+        storage[indexA] = storage[indexB];
+        storage[indexB] = tmp;
+    }
+
+    public void putArray(Integer[] array) {
+        storage = Arrays.copyOf(array, array.length);
+    }
+
 }
--- src/test/java/com/example/course2lesson17215algorithmspart2/MyIntegerListTest.java
+++ src/test/java/com/example/course2lesson17215algorithmspart2/MyIntegerListTest.java
@@ -3,6 +3,7 @@ package com.example.course2lesson17215algorithmspart2;
 import com.example.course2lesson17215algorithmspart2.implementations.MyIntegerList;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -36,6 +37,17 @@ public class MyIntegerListTest {
     }
 
     @Test
+    public void addByIndex(){
+        MyIntegerList testIntegerList = randomlyFilledIntegerList(5) ;
+        int testIndex = testIntegerList.size()-1;
+        Integer testItem = random().nextInt(1000000,2000000) ;
+        testIntegerList.add(testIndex,testItem);
+        assertEquals(testItem, testIntegerList.add(testIndex, testItem));
+        assertEquals(testItem, testIntegerList.get(testIndex));
+
+    }
+
+    @Test
     public void removeByItemTest() {
 
         MyIntegerList testIntegerList = new MyIntegerList(5) ;
@@ -51,7 +63,58 @@ public class MyIntegerListTest {
 
             assertEquals(testItem, testIntegerList.remove(testItem));
             assertEquals(null,testIntegerList.remove(testItem));
-
         }
 
+        @Test
+        public void removeByIndexTest() {
+
+        MyIntegerList testIntegerList = randomlyFilledIntegerList(5) ;
+
+            System.out.println("testIntegerList.size() = " + testIntegerList.size());
+            int testIndex = random().nextInt(0, testIntegerList.size());
+        Integer testItem = random().nextInt(1000000, 2000000) ;
+        while (testIntegerList.contains(testItem)) {
+            testItem = random().nextInt(1000000, 2000000) ;
+        }
+        testIntegerList.set(testIndex, testItem) ;
+        assertEquals(testItem, testIntegerList.remove(testIndex));
+        assertNotEquals(testItem, testIntegerList.get(testIndex));
+        }
+
+        @Test
+        public void sortByInsertionTest() {
+        MyIntegerList testIntegerList = randomlyFilledIntegerList(100000) ;
+            long start = System.currentTimeMillis();
+            testIntegerList.sortByInsertion();
+            System.out.println(System.currentTimeMillis() - start);
+        } //13750ms, 12933ms, 12856ms
+
+        @Test
+        public void sortBySelectionTest() {
+        MyIntegerList myIntegerList = randomlyFilledIntegerList(100000) ;
+            long start = System.currentTimeMillis();
+            myIntegerList.sortByInsertion();
+            System.out.println(System.currentTimeMillis() - start);
+            // 13635 ms, 13223ms, 13274ms
+        }
+
+        @Test
+        public void bubbleSortTest() {
+            MyIntegerList myIntegerList = randomlyFilledIntegerList(100000) ;
+            long start = System.currentTimeMillis();
+            myIntegerList.bubbleSort();
+            System.out.println(System.currentTimeMillis() - start);
+            // 31578ms, 31688ms, 31529ms
+        }
+
+    private MyIntegerList randomlyFilledIntegerList(int size) {
+        MyIntegerList integerList = new MyIntegerList(size);
+
+        for (int i = 0; i < size; i++) {
+            integerList.add(random().nextInt(0, 10000)) ;
+        }
+        return integerList ;
     }
+
+
+}
